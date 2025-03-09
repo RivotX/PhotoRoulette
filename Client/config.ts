0ba3ce78c546f3b8b9858680716e1218c3464c3a
Miguel <miguelrosso60@gmail.com>
2025-03-09 23:35:47 +0100

    [CONFIG] config.ts for expo apk generator

---
+++ Client/config.ts
@@ -0,0 +1,8 @@
+// You need to rename this file to config.ts and replace the SERVER_URL with your local IP address (http://(IP):3000/)  
+const getEnvVars = () => {
+  return {
+    SERVER_URL: "" // secret :3 (please hire Geyson Steven Gualdron, https://www.linkedin.com/in/geysongualdron)
+  };
+};
+
+export default getEnvVars;
--- Client/configExample.ts
+++
@@ -1,8 +0,0 @@
-// You need to rename this file to config.ts and replace the SERVER_URL with your local IP address  
-const getEnvVars = () => {
-  return {
-    SERVER_URL: "http://ipv4:3000", // Change this to http://(IP):3000
-  };
-};
-
-export default getEnvVars;
